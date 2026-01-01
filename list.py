72dbea60e6beffc94bd3b7e4be39aad2546891c1
NavneetSharma333 <navneet240408@gmail.com>
2026-01-01 23:45:28 +0530

    ListFlowAssignments

--- list.py
+++ list.py
@@ -9,3 +9,10 @@ print(browsers)
 
 remained_items = browsers.pop()
 print(f"removed value is {remained_items},  remained list is  {browsers}")
+
+new_list = []
+for i in browsers:
+    updated_dataset = i.upper()
+    new_list.append(updated_dataset)
+
+print(f"now list with uppercases : {new_list}")
