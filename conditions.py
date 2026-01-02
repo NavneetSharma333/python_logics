3971c3cf7aae059c3b1bf99a9b3085a2794a060a
NavneetSharma333 <navneet240408@gmail.com>
2026-01-03 01:10:22 +0530

    addedForLoop

--- conditions.py
+++ conditions.py
@@ -10,27 +10,27 @@ else:
 # Print numbers 1–20
 trgt_int = 13
 
-# for i in range(1, 21):
-#     if i % 2 != 0:
-#         print(i)
-
-#     if i == trgt_int:
-#         print(f"the target {trgt_int} is found")
-#         break
-# else:
-#     print(f"target {trgt_int} not found")
-
-
-i = 0
-while i < 20:
-    i = i+1
-    #print(i)
-    
+for i in range(20):
     if i % 2 != 0:
         print(i)
-    
+
     if i == trgt_int:
-        print(f"{trgt_int} is found form while loop also")
+        print(f"the target {trgt_int} is found")
         break
 else:
-    print(f"{trgt_int} not found form while loop")
+    print(f"target {trgt_int} not found")
+
+
+# i = 0
+# while i < 20:
+#     i = i+1
+#    # print(i)
+    
+#     if i % 2 != 0:
+#         print(i)
+    
+#     if i == trgt_int:
+#         print(f"{trgt_int} is found form while loop also")
+#         break
+# else:
+#     print(f"{trgt_int} not found form while loop")
