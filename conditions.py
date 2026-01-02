4b2bcbd341e1daea603dff2ad3acc84cc94103e8
NavneetSharma333 <navneet240408@gmail.com>
2026-01-03 01:07:56 +0530

    loopConditionsWhile

--- conditions.py
+++ conditions.py
@@ -1,4 +1,3 @@
-
 browser = "chrome"
 
 if browser == "chrome":
@@ -7,3 +6,31 @@ elif browser == "firefox":
     print("Mozilla browser")
 else:
     print("Unsupported browser")
+
+# Print numbers 1–20
+trgt_int = 13
+
+# for i in range(1, 21):
+#     if i % 2 != 0:
+#         print(i)
+
+#     if i == trgt_int:
+#         print(f"the target {trgt_int} is found")
+#         break
+# else:
+#     print(f"target {trgt_int} not found")
+
+
+i = 0
+while i < 20:
+    i = i+1
+    #print(i)
+    
+    if i % 2 != 0:
+        print(i)
+    
+    if i == trgt_int:
+        print(f"{trgt_int} is found form while loop also")
+        break
+else:
+    print(f"{trgt_int} not found form while loop")
