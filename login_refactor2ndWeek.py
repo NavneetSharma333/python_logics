25ff55e7a4694b7738695d2b73eb7bc6bb6eb674
NavneetSharma333 <navneet240408@gmail.com>
2026-05-16 13:48:43 +0530

    fixes_in_lofin_refactored

--- login_refactor2ndWeek.py
+++ login_refactor2ndWeek.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 
+# Test users
 users = [
     ("standard_user", "secret_sauce"),
     ("error_user", "secret_sauce"),
@@ -11,52 +12,74 @@ users = [
     ("visual_user", "pass3")
     ]
 
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-driver.maximize_window()
+#Setup driver
+class Launch_driver_and_get_website:
+    def driver_lauch(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.saucedemo.com/")
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 12)
+        
+#login page class
+class Login_flow:
+    def __init__(self, driver, wait):
+        self.driver = driver
+        self.wait = wait
+        
+    def user_login(self, username, password):
+        for username, password in users:
+            
+            # Locating the username field
+            username_input = self.wait.until(EC.element_to_be_clickable((By.ID, "user-name")))
+            #locating password field
+            password_input = self.driver.find_element(By.ID, "password")
+            #locating the login button
+            login_button = self.driver.find_element(By.ID, "login-button")
+            
+            #clear exiting texts
+            username_input.clear()
+            password_input.clear()
+            
+            #entering the username and password
+            username_input.send_keys(username)
+            password_input.send_keys(password)
+            #clicking login button
+            login_button.click()
+            
+ #logout page class      
+class Logout_class(Login_flow):
+    def __init__(self, driver, wait):
+        super().__init__(driver, wait)
+        self.driver = driver
+        self.wait = wait
+        
+    def logging_out_method(self):
+        # url_after_login = "https://www.saucedemo.com/inventory.html"
+        #if self.driver.current_url == url_after_login:
+        #    print(self.username, "Pass")
 
-wait = WebDriverWait(driver, 12)
-
-def user_login():
-    for username, password in users:
-        driver.get("https://www.saucedemo.com/")
-        username_input = wait.until(EC.element_to_be_clickable((By.ID, "user-name")))
-        password_input = driver.find_element(By.ID, "password")
-        login_button = driver.find_element(By.ID, "login-button")
-        
-        username_input.clear()
-        password_input.clear()
-        
-        username_input.send_keys(username)
-        password_input.send_keys(password)
-        login_button.click()
-        
-        #time.sleep(3)
-        print(driver.current_url)
-        
-        url_after_login = "https://www.saucedemo.com/inventory.html"
-        
-    def logging_out():
-        if driver.current_url == url_after_login:
-            print(username, "Pass")
-        
-            hamburger_menu = wait.until(EC.presence_of_element_located((By.ID,"react-burger-menu-btn"))).click()
+            # clicking hamburger menu
+            hamburger_menu = self.wait.until(EC.presence_of_element_located((By.ID,"react-burger-menu-btn"))).click()
             #driver.find_element(By.ID, "react-burger-menu-btn")
             #hamburger_menu.click()
 
             #wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "bm-menu-wrap")))
-        
-            logout_button = wait.until(EC.visibility_of_element_located((By.ID,"logout_sidebar_link")))#.click()
+            
+            #clicking logout button
+            logout_button = self.wait.until(EC.visibility_of_element_located((By.ID,"logout_sidebar_link")))#.click()
             #driver.find_element(By.ID, "logout_sidebar_link")
             logout_button.click()
             print(driver.current_url)
             
             #wait.until(EC.presence_of_element_located((By.ID, "user-name")))
         
-        else:
-            print(username, "fail")
-            
-user_login()
+        #else:
+        #    print(username, "fail")
+        
+lauch_driver = Launch_driver_and_get_website()
+login = Login_flow()
+logging_out = Logout_class()
+
     
     #homepage_text = driver.find_element(By.XPATH, "//span[@class='title']")
     
